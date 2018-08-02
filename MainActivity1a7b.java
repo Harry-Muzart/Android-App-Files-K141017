9244b83b7dc0172edf13faa141a4e6a7825479ef
Harry Muzart <harry.muzart.12@alumni.ucl.ac.uk>
2018-08-02 08:39:30 +0100

    Add files via upload

---
+++ MainActivity1a7b.java
@@ -0,0 +1,16 @@
+package com.example.harrymuzart.myapplication14oct2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity1a7b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity1a7b);
+    }
+
+
+
+}
